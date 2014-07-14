e4d073dfe3d618df71394acb43bd1fa6d0a80bbc
Adam Gibbons <adam@blowmedia.co.uk>
2014-07-14 17:27:48 +0100

    site on dev server

--- wp-config.php
+++ wp-config.php
@@ -8,7 +8,7 @@ if ( !defined('ABSPATH') )
   define('ABSPATH', dirname(__FILE__) . '/');
  
 /** Include environment specific configuration. */
-define('WP_ENV', (getenv('WP_ENV') ?: 'production'))
+define('WP_ENV', (getenv('WP_ENV') ?: 'production'));
 require_once(ABSPATH . 'wp-config-' . WP_ENV . '.php');
  
 /** Sets up WordPress vars and included files. */
