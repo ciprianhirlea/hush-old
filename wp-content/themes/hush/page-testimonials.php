17cdb2161157cdf0dee7bdcb308d318590386407
Adam Gibbons <adam@blowmedia.co.uk>
2014-07-15 16:13:52 +0100

    site amends

---
+++ wp-content/themes/hush/page-testimonials.php
@@ -0,0 +1,59 @@
+<?php get_header(); ?>
+	
+	<!-- section -->
+	<section role="main">
+
+		<div class="row">
+
+			<div class="large-12 small-12 columns no-padding internal-header">
+				<?php 
+					if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+  					the_post_thumbnail();
+				} 
+				?>
+			</div>
+
+		</div>
+
+		<div class="row">
+
+			<div class="large-8 small-12 columns no-padding">
+				
+				<div id="internal-content">
+
+					 <?php $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+$args = array( 'post_type' => 'testimonials', 'posts_per_page' => 5, 'paged' => $paged );
+$wp_query = new WP_Query($args);
+while ( have_posts() ) : the_post(); ?>
+
+						<?php the_content() ?>
+
+						
+						<hr/>
+
+					<?php endwhile ?>
+
+					<?php site_pagination(); ?>
+
+				</div>
+
+			</div>
+
+			<div class="large-4 small-12 columns no-padding">
+				
+				<div id="side-content-blog">
+
+					
+
+				</div>
+
+			</div>
+
+		</div>	
+	
+		<?php include (TEMPLATEPATH . '/includes/bottom-content.php'); ?>
+
+	</section>
+	<!-- /section -->
+
+<?php get_footer(); ?>
