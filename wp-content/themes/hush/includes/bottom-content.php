d5e299d1f0ebbc1e7eed9e4c2c4b5fc5bfa3d280
Adam Gibbons <adam@blowmedia.co.uk>
2014-07-15 14:24:52 +0100

    site amends

--- wp-content/themes/hush/includes/bottom-content.php
+++ wp-content/themes/hush/includes/bottom-content.php
@@ -92,11 +92,10 @@
 		<div class="large-12 small-12 no-padding">
 
 			<div class="featured-testimonial-text">
-				<span>
 				<?php 
              					$loop = new WP_Query( array('post_type' => array('featured_testimonial')) ); ?>
 					<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
-				
+				<span>
 				<div class="cycle-slideshow">
 				<?php the_content() ?>
 				</div>
