6fef5b8771d5eda7d7698d9401bdac28a7b148ee
Adam Gibbons <adam@blowmedia.co.uk>
2014-07-15 15:02:16 +0100

    site amends

--- wp-content/themes/hush/includes/bottom-content.php
+++ wp-content/themes/hush/includes/bottom-content.php
@@ -90,17 +90,21 @@
 
 	<div class="row">
 
-		<div class="large-12 small-12 no-padding">
-			<?php 
+		<?php 
              					$loop = new WP_Query( array('post_type' => array('featured_testimonial')) ); ?>
 					<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+
+		<div class="large-12 small-12 no-padding">
+			
 				
 			
 				<?php the_content() ?>
 			
 
-			<?php endwhile; ?>
+			
 		</div>
+
+		<?php endwhile; ?>
 
 	</div>
 
