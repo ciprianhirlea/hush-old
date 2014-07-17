fa232f678ae4821a97b98a5ca27ad78b33aeb91c
Adam Gibbons <adam@blowmedia.co.uk>
2014-07-17 09:44:57 +0100

    site amends

--- wp-content/themes/hush/includes/bottom-content.php
+++ wp-content/themes/hush/includes/bottom-content.php
@@ -345,15 +345,17 @@
 			<div class="featured-testimonial-text">
 
 				<div class="testimonialBox">
-					<?php $loop = new WP_Query( array('post_type' => array('featured_testimonial',),) ); ?>
-					<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
 
 					<div class="cycle-slideshow"  data-cycle-fx="scrollHorz"  data-cycle-timeout="2000" data-cycle-slides="> div">
+					<?php $loop = new WP_Query( array('post_type' => array('featured_testimonial',),) ); ?>
+					<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
 						<div>
 							<p><?php the_content() ?></p>
 						</div>
-					</div>
+					
 					<?php endwhile; ?>
+
+					</div>
 				
 				</div>
 
