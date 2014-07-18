67c7686c0e0c0e05a0baa60c198d4f86f8398760
Adam Gibbons <adam@blowmedia.co.uk>
2014-07-18 10:59:34 +0100

    site amends

--- wp-content/themes/hush/404.php
+++ wp-content/themes/hush/404.php
@@ -4,19 +4,37 @@
 	<section role="main">
 	
 		<!-- article -->
-		<article id="404" <?php post_class(); ?>>
+
+		<div class="row">
+
+			<div class="large-12 small-12 columns no-padding">
+				<div id="slider" class="nivoSlider">
+				    <img src="<?php echo get_template_directory_uri(); ?>/img/slides/slide-1.jpg" alt="" />
+				</div>
+			</div>
+
+		</div>
+
+		<div class="row">
+
+			<div class="large-12 small-12 columns no-padding">	
+				<article id="404" <?php post_class(); ?>>
 		
-			<h1>404</h1>
-			<h2>
-				<a href="<?php echo home_url(); ?>">Home</a>
-			</h2>
+					<h1>404 </h1>
+					<h2>
+					We are sorry but we cannot find what you are looking for
+					</h2>
 			
-		</article>
+				</article>
+
+			</div>
+
+		</div>
 		<!-- /article -->
+		
+		<?php include (TEMPLATEPATH . '/includes/bottom-content.php'); ?>
 		
 	</section>
 	<!-- /section -->
-	
-<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
